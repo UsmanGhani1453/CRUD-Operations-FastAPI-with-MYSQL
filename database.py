dba27f89af5bd9a18293663e21db4b55cd1137a7
UsmanGhani1453 <188512679+UsmanGhani1453@users.noreply.github.com.>
2026-07-13 15:31:07 +0500

    Updated database URL to accept cloud environment variables

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = os.getenv(
     "DATABASE_URL", 
-    "mysql+pymysql://root:your_local_password@localhost:3306/haak_db" 
+    "mysql+pymysql://root:password@localhost:3306/haak_db" 
 )
 if SQLALCHEMY_DATABASE_URL.startswith("mysql://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
